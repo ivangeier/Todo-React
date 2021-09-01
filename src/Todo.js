4f31c761a8bdd664d198355792df8c73d6578976
Ivan Geier <vilajr.ivan@hotmail.com>
2021-09-01 14:19:14 -0300

    add Class Item to organize the list

--- src/Todo.js
+++ src/Todo.js
@@ -1,14 +1,17 @@
 import React, { useState } from 'react';
 import List from './List';
 import TodoForm from './TodoForm';
+import Item from './Item';
 import './Todo.css';
 
 function Todo() {
 
     const [items, setItems] = useState([]);
 
-    function onAddItem(item) {
+    function onAddItem(text) {
+        let item = new Item(text);
         setItems([...items, item]);
+        console.log(items);
     }
 
     return (
